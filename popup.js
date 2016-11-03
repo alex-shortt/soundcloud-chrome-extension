39a154d7ddf623e54f22cfaa5885f48b7f9ae9da
Shortt <alexander.shortt18@bcp.org>
2016-11-03 08:16:39 -0700

    setting up development environment

---
+++ popup.js
@@ -0,0 +1,28 @@
+function getCurrentTabUrl(callback) {
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
+
+    chrome.tabs.query(queryInfo, function (tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        console.assert(typeof url == 'string', 'tab.url should be a string');
+        callback(url);
+    });
+}
+
+function hideListing() {
+    document.getElementsByClassName("soundTitle__title")[3].innerHTML = "test123";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    getCurrentTabUrl(function (url) {
+        document.body.innerHTML = url.split('/')[2];
+        var webName = url.split('/')[2];
+        if (webName == "soundcloud.com") {
+            chrome.tabs.executeScript(null, {file: "scripts/hide-listing.js"});
+            chrome.tabs.executeScript('console.log("' + webName + '");');
+        }
+    });
+});
---
+++ scripts/hide-listing.js
@@ -0,0 +1,6 @@
+console.log("running...");
+var listings = document.getElementsByClassName("soundList__item");
+
+for(var i = 0; i < listings.length; i++){
+    console.log(listings[i]);
+}
