e7ce566229e20f1467c42a854421cd22ed37c00f
Shortt <alexander.shortt18@bcp.org>
2016-12-07 16:47:25 -0800

    success- will send proper link to heroku on button click with the right information to download the song

--- scripts/hide-listing.js
+++ scripts/hide-listing.js
@@ -2,18 +2,58 @@ console.log("running...");
 var listings = document.getElementsByClassName("soundList__item");
 
 
-function testThis(element) {
-    element.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode.parentNode.parentNode);
+function deleteListing(element) {
+    var songListing = element.parentNode.parentNode.parentNode.parentNode;
+    songListing.parentNode.removeChild(songListing);
+}
+
+function downloadSong(element) {
+    var songListing = element.parentNode.parentNode.parentNode.parentNode;
+    var clientId = "pPmFkm7w8XvU1oRdViIbG2nMmhimho6K";
+    var title, artist, genre, art, link;
+
+    link = songListing.getElementsByClassName("soundTitle__title")[0].href;
+
+    var xhr = new XMLHttpRequest();
+    console.log(link);
+    xhr.open('GET', "https://api.soundcloud.com/resolve.json?url=" + encodeURIComponent(link) + "&client_id=" + encodeURIComponent(clientId), false);
+    xhr.send();
+
+    if (xhr.responseText == "") {
+        //scrape it
+        console.log("This track does not work!");
+        return false;
+    } else {
+        var result = JSON.parse(xhr.responseText);
+        console.log(result);
+
+        title = result.title;
+        artist = result.user.username;
+        genre = result.genre;
+        art = result.artwork_url.replace("large", "t500x500");
+    }
+
+    console.log(artist + " - " + title + " >> " + genre + " >> " + art);
+    var download_link = "https://soundcloud-downloader.herokuapp.com/getSound?link=" + link.replace("https", "http") + "&artist=" + artist + "&title=" + title + "&genre=" + genre + "&album=" + title + "&album_art=" + art;
+
+    /*
+    var downloadSong = new XMLHttpRequest();
+    downloadSong.open('GET', "https://soundcloud-downloader.herokuapp.com/getSound?link=" + link.replace("https", "http") + "&artist=" + artist + "&title=" + title + "&genre=" + genre + "&album=" + title + "&album_art=" + art, false);
+    downloadSong.send();
+    */
+
+    var win = window.open(download_link, '_blank');
+
+    return false;
 }
 
 for (var i = 0; i < listings.length; i++) {
     console.log(listings[i]);
     var titleBar = listings[i].getElementsByClassName("g-flex-row-centered")[0];
     var hideButton = document.createElement("a");
-    hideButton.className = "fa fa-times ext-hideButton";
-    hideButton.href = "#";
+    hideButton.className = "fa fa-cloud-download ext-hideButton";
     hideButton.onclick = function () {
-        testThis(this);
+        return downloadSong(this);
     }
     titleBar.appendChild(hideButton);
 }
