fb195ab90f769d71766a9235d0edec62ddf04fe7
alex-shortt <alexander.shortt@gmail.com>
2017-01-26 09:02:25 -0800

    Made a logo/promotional tiles, preparing for first launch...

--- popup.js
+++ popup.js
@@ -1,24 +1,2 @@
-function getCurrentTabUrl(callback) {
-    var queryInfo = {
-        active: true,
-        currentWindow: true
-    };
-
-    chrome.tabs.query(queryInfo, function (tabs) {
-        var tab = tabs[0];
-        var url = tab.url;
-        console.assert(typeof url == 'string', 'tab.url should be a string');
-        callback(url);
-    });
-}
-
-function hideListing() {
-    document.getElementsByClassName("soundTitle__title")[3].innerHTML = "test123";
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-    getCurrentTabUrl(function (url) {
-        document.body.innerHTML = url.split('/')[2];
-    });
-    chrome.tabs.executeScript('console.log("' + webName + '");');
 });
